ea4eb0bff9777e48e7421be6726c747c997edb79
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-02-22 23:14:32 +0700

    feat: create and implement the dot (titik) object

---
+++ src/Dot.py
@@ -0,0 +1,24 @@
+import random
+import math
+
+class Dot:
+    def __init__(self, dimension):
+        # Initiate the number of dimension of dot and its position
+        self.dimension = dimension
+        self.position = [random.randint(-1000, 1000) for i in range (dimension)]
+    
+    def __sub__(self, other):
+        # Calculate the distance between this dot and other dot
+        distance_squared = 0
+        
+        for i in range(0, self.dimension):
+            distance_squared += math.pow((other.position[i]-self.position[i]), 2)
+
+        return math.sqrt(distance_squared)
+    
+    def printDot(self):
+        # Print the position of this dot
+        print(f"[{self.position[0]}", end="")
+        for i in range(1, self.dimension):
+            print(f", {self.position[i]}", end="")
+        print("]", end="")
