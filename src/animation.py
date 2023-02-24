aca18dfe2544dcdf207abbf56736f427abf54fcd
AustinPardosi <gabrielpardosi26@gmail.com>
2023-02-24 13:48:00 +0700

    Add diff color for chosen pair of dots, and others

--- src/animation.py
+++ src/animation.py
@@ -1,23 +1,38 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
-# Generate some 3D data
-x = np.random.normal(size=50)
-y = np.random.normal(size=50)
-z = np.random.normal(size=50)
+from ListDot import * 
 
 # Create a 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Plot the dots using scatter plot
-ax.scatter(x, y, z, c='r', marker='o')
+# Generate the dots randomly
+dotList = ListDot(3, 100) 
+dotList.generateRandom()
 
-# Set the labels for the axes
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+# Generate some 3D data
+# x = ax.set_xticks(dotList.)
+# y = ax.set_yticks(dotList.dimension)
+# z = ax.set_zticks(dotList.dimension)
+x = np.random.normal(size=50)
+y = np.random.normal(size=50)
+z = np.random.normal(size=50)
+
+# Creating the title
+ax.set_title("Pasangan Titik Terdekat 3D")
+
+# Plot the dots using scatter plot
+ax.scatter(x, y, z, c='blue', marker='o')
+
+# Plot the nearest dots using scatter plot
+ax.scatter(2, 3, 4, c='red', marker='x')
+ax.scatter(2, 3, 5, c='red', marker='x')
+
+# Set the labels name for the axes
+ax.set_xlabel('Sumbu X')
+ax.set_ylabel('Sumbu Y')
+ax.set_zlabel('Sumbu Z')
 
 # Show the plot
 plt.show()
